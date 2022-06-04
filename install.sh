a3d71402aa620ad28768e2628f742aa672bdfdd2
Seal-Clubber <35927816+Seal-Clubber@users.noreply.github.com>
2022-06-04 20:42:11 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -39,13 +39,12 @@ sudo chown -R $USER:$GROUP ~/.nvm
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-nvm install v8.17.0
-nvm use v8.17.0
+nvm install v12.13.0
+nvm use v12.13.0
 npm update -g
 
 npm install -g webpack@4.46.0
 npm install -g pm2@4.5.6
-npm install -g npm@latest-6
 
 npm install
 npm update
