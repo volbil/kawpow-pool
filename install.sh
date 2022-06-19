30564d319ef454a562be45d16e61463ca02bece8
Seal-Clubber <35927816+Seal-Clubber@users.noreply.github.com>
2022-06-20 00:00:01 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -37,14 +37,14 @@ sudo systemctl start ntp
 
 sudo rm -rf ~/.nvm
 sudo rm -rf ~/.npm
-wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 source ~/.bashrc
 sudo chown -R $USER:$GROUP ~/.nvm
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-nvm install v18.4.0
-nvm use v18.4.0
+nvm install v12.22.6
+nvm use v12.22.6
 
 npm update -g
 npm install -g pm2@latest
